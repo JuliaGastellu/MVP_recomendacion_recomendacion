0c9d182335e0c0cad7921bdcd593a28d949ab85e
Julia Gastellu <juliacgastellu@gmail.com>
2024-09-11 07:03:16 -0300

    correccion

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.responses import HTMLResponse
 from enum import Enum
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from express import require
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
